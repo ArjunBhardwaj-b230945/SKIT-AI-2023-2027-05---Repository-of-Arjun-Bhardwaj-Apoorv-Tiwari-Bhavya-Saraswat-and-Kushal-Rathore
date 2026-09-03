b6bc00f6afaa8690d76bbc241cd89cc595ed54b5
Apoorv Tiwari <b230708@skit.ac.in>
2026-09-03 23:10:29 +0530

    backend phase-1

---
+++ routes/auth.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const { Signin, Signup } = require("../controllers/auth");
+
+const router = express.Router();
+
+router.post("/signin", Signin);
+
+router.post("/signup", Signup);
+
+module.exports = router;
